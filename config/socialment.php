f75149ab1020dd270285c2286d6346f3079fc127
Chris Reed <chris@reedtech.us>
2023-08-28 06:29:50 -0500

    Updated configuration file.

--- config/socialment.php
+++ config/socialment.php
@@ -2,10 +2,23 @@
 
 // config for ChrisReedIO/Socialment
 return [
-    'providers' => [
+	'view' => [
+		// Set the text above the provider list
+		'prompt' => 'Or Login Via',
+		// Or change out the view completely with your own
+		'providers-list' => 'socialment::providers-list',
+	],
+	'routes' => [
+		'home' => 'filament.admin.pages.dashboard',
+	],
+	'models' => [
+		// If you want to use a custom user model, you can specify it here.
+		'user' => \App\Models\User::class,
+	],
+	'providers' => [
         'azure' => [
-        	'icon' => 'azure',
-        	'label' => 'Azure',
+        	'icon' => 'fab-microsoft',
+        	'label' => 'Azure Active Directory',
         ]
-    ],
+	],
 ];
