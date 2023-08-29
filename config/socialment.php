491903da16579daa60e3a41659f0842c67a6d59d
Chris Reed <chris@reedtech.us>
2023-08-28 21:04:15 -0500

    refactored config

--- config/socialment.php
+++ config/socialment.php
@@ -2,23 +2,30 @@
 
 // config for ChrisReedIO/Socialment
 return [
+	// The list of providers to display on the login page
+	// You must install the appropriate Socialite provider package for each provider you want to use
+	// if it isn't one supported by the core Laravel Socialite package.
+	'providers' => [
+		// Use the key based on the provider's documentation
+		'azure' => [
+			'icon' => 'fab-microsoft', // Font Awesome icon class
+			'label' => 'Azure Active Directory', // The label to display for the provider
+		]
+	],
+
 	'view' => [
 		// Set the text above the provider list
 		'prompt' => 'Or Login Via',
 		// Or change out the view completely with your own
 		'providers-list' => 'socialment::providers-list',
 	],
+
 	'routes' => [
 		'home' => 'filament.admin.pages.dashboard',
 	],
+
 	'models' => [
 		// If you want to use a custom user model, you can specify it here.
 		'user' => \App\Models\User::class,
-	],
-	'providers' => [
-        'azure' => [
-        	'icon' => 'fab-microsoft',
-        	'label' => 'Azure Active Directory',
-        ]
 	],
 ];
